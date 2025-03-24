9e965f2a545f886a96d4ec1073c5bd87e85d37ff
RuslanqQA <marshtupa.ruslan@gmail.com>
2025-03-24 20:05:10 +0200

    Update js_homework7-1.js

--- js_homework7-1.js
+++ js_homework7-1.js
@@ -2,4 +2,4 @@ const regex = /\b[^Aa\s]{6,}\b/g;
 const text = "Wonderful , Joyful , Excited , Mystery , Develop , Happiness , Amazing , Task , Apple , Time";
 const matches = text.match(regex);
 
-console.log(matches); // Виведе: [ 'Wonderful', 'Joyful' ]
+console.log(matches); // Виведе: [ 'Wonderful', 'Joyful', 'Excited', 'Mystery', 'Develop' ]
